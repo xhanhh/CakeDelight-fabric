8c3fd4989e607eb240b773ca706be109a965a280
‘xhanhh’ <myxhan@outlook.com>
2025-06-17 20:43:13 +0800

    modify: 稍微调整食物数值

--- src/main/java/top/ilov/mcmods/cakedelight/items/CDFoodComponents.java
+++ src/main/java/top/ilov/mcmods/cakedelight/items/CDFoodComponents.java
@@ -12,7 +12,7 @@ public class CDFoodComponents {
     public static final FoodComponent tomato_egg_noodle_soup = new FoodComponent.Builder().nutrition(5).saturationModifier(0.4F).build();
     public static final FoodComponent cream_of_mushroom_soup = new FoodComponent.Builder().nutrition(6).saturationModifier(0.4F).build();
 
-    public static final FoodComponent tomatoes_with_tomatoes = new FoodComponent.Builder().nutrition(4).saturationModifier(0.2F).build();
+    public static final FoodComponent tomatoes_with_tomatoes = new FoodComponent.Builder().nutrition(4).saturationModifier(0.4F).build();
     public static final FoodComponent stewed_beef_with_tomato = new FoodComponent.Builder().nutrition(6).saturationModifier(0.5F).build();
 
 }
